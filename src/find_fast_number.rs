4b0a86a5e92c6f9b347a37bd129a7925a34c6789
James Millard <millard.jn@gmail.com>
2022-10-16 16:18:39 +1000

    Fix bug where minimum fft length product was not updated

--- src/find_fast_number.rs
+++ src/find_fast_number.rs
@@ -1,23 +1,36 @@
 use std::cmp::min;
 
+/// Find a fft length larger that is at least `minimum_size` in length that is made up of a limited set of factors.
+/// The `once_factor` will be used exactly once, the multi factors may be used any number of times to reach the minimum.
 fn fast_fft_len(
-    lower_limit: usize,
-    single_factor: usize,
+    minimum_len: usize,
+    once_factor: usize,
     multi_factor1: usize,
     multi_factor2: usize,
 ) -> usize {
-    let mut product = single_factor;
-    while product < lower_limit {
+    assert!(once_factor >= 1);
+    assert!(multi_factor1 > 1);
+    assert!(multi_factor2 > 1);
+
+    // Apply once factor
+    let mut product = once_factor;
+
+    // apply second factor until at or above minimum
+    while product < minimum_len {
         product *= multi_factor2
     }
-    let mut min = product;
+
+    // remove second factor one at a time then add enough first factor to reach minimum_size
+    // repeat while tracking lowest viable product
+    let mut best = product;
     loop {
-        match product.cmp(&lower_limit) {
+        match product.cmp(&minimum_len) {
             std::cmp::Ordering::Less => product *= multi_factor1,
-            std::cmp::Ordering::Equal => min = product,
+            std::cmp::Ordering::Equal => return product,
             std::cmp::Ordering::Greater => {
+                best = min(best, product);
                 if product % multi_factor2 != 0 {
-                    return min;
+                    return best;
                 }
                 product /= multi_factor2;
             }
